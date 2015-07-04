82744d2720beaaa196a9ac5898d9c48584122253
Victor Aponte <victor@victoraponte.org>
2015-07-04 13:33:52 -0700

    working on slide transitions

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+/* global console */
+/* jshint unused:false, evil: true*/
 var cssSlides = document.querySelectorAll(".slide-css-code");
 var jsSlides = document.querySelectorAll(".slide-js-code");
 var allSlides = document.querySelectorAll(".slide");
+var theDocument = document.body;
 
 [].forEach.call(allSlides, function(slide, slideNumber, slides){
   slide.setAttribute('data-slide-number', slideNumber + 1);
@@ -17,5 +20,62 @@ var allSlides = document.querySelectorAll(".slide");
   cssCode.addEventListener("blur", function(){
     var theStyles = cssCode.innerText;
     cssCode.innerHTML = theStyles;
+  });
 });
-});
+
+function moveToPreviousSlide() {
+  // if we are already at the first slide, do nothing, for now...
+  var currentSlide = document.querySelector(".slide.current");
+  var nextSlide = currentSlide.nextElementSibling;
+  var previousSlide = currentSlide.previousElementSibling;
+  console.dir(currentSlide);
+  console.dir(nextSlide);
+  console.dir(previousSlide);
+
+  if (!previousSlide) {
+    // do nothing
+    return;
+  } else {
+    
+  }
+
+  // otherwise move to the previous slide.
+
+}
+
+function moveToNextSlide() {
+
+}
+
+function handleKeyDown(event){
+  var LEFT_ARROW = 37;
+  var UP_ARROW = 38;
+  var RIGHT_ARROW = 39;
+  var DOWN_ARROW = 40;
+  var theKey;
+  var theTarget;
+
+  if(typeof event === 'undefined') {
+    event = window.event;
+  }
+
+  theTarget = event.target;
+  // ignore arrow keys coming from the editable content elements
+  if ('contenteditable' in theTarget.attributes) {
+    return;
+  }
+
+  theKey = event.key || event.keyCode;
+
+  if (theKey === LEFT_ARROW || theKey === UP_ARROW) {
+    moveToPreviousSlide();
+  }
+
+  if (theKey === LEFT_ARROW || theKey === UP_ARROW) {
+    moveToPreviousSlide();
+  }
+  console.log(theKey);
+
+}
+// bind the keypress events to the body
+document.body.addEventListener("keydown", handleKeyDown, false);
